0546a03004e253525e45e35441d2e4822965da73
Kristýna Kopečková <kopeckova92@gmail.com>
2021-05-01 11:50:15 +0200

    Navigace se schovává při kliknutí na odkaz

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import './style.css';
 
 console.log('funguju!');
+
+const navElm = document.querySelector('nav');
+const navBtn = document.querySelector('#nav-btn');
+
+const showNav = () => {
+  navElm.classList.toggle('nav-closed');
+};
+
+navBtn.addEventListener('click', showNav);
+
+const navAllElm = document.querySelectorAll('nav a');
+navAllElm.forEach((elm) => {
+  elm.addEventListener('click', showNav);
+});
