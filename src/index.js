51925d063fca96958ae85ed9dbf77f9ae99fcfea
Kristýna Kopečková <kopeckova92@gmail.com>
2021-05-01 14:16:25 +0200

    Vytvořena komponenta Layers

--- src/index.js
+++ src/index.js
@@ -34,3 +34,61 @@ const changeText = () => {
 };
 
 btnElm.addEventListener('click', changeText);
+
+const menu = [
+  {
+    color: '#feeeca',
+    label: 'mléčná pěna',
+  },
+];
+
+const Layer = (props) => {
+  return `
+    <div class="layer">
+    <div class="layer__color" style="background-color:${props.color}"></div>
+    <div class="layer__label">${props.label}</div>
+`;
+};
+
+const drinkSection = document.querySelector('.drink__info');
+
+const item = [
+  {
+    color: '#feeeca',
+    label: 'mléčná pěna',
+  },
+  {
+    color: '#fed7b0',
+    label: 'teplé mléko',
+  },
+  {
+    color: '#613916',
+    label: 'espresso',
+  },
+];
+
+for (let i = 0; i < item.length; i++) {
+  drinkSection.innerHTML += Layer(item[i]);
+}
+
+//  <div class="layer">
+//    <div
+//      class="layer__color"
+//      style="background-color: #feeeca"
+//    ></div>
+//    <div class="layer__label">mléčná pěna</div>
+//  </div>
+//  <div class="layer">
+//    <div
+//      class="layer__color"
+//      style="background-color: #fed7b0"
+//    ></div>
+//    <div class="layer__label">teplé mléko</div>
+//
+//  <div class="layer">
+//    <div
+//      class="layer__color"
+//      style="background-color: #613916"
+//    ></div>
+//    <div class="layer__label">espresso</div>
+//  </div>
